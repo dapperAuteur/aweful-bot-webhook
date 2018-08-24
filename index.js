907a3ffd2b45403e7aae9577a159d0346c1eefa6
dapperauteur <a@awews.com>
2018-08-24 16:45:13 -0400

    log request_body.message

--- index.js
+++ index.js
@@ -132,7 +132,7 @@ function callSendAPI(sender_psid, response) {
     }, (err, res, body) => {
         if (!err) {
             console.log(`message sent! ${ request_body }`);
-            console.log(`${ request_body.message }`);
+            console.log("request_body.message");
         } else {
             console.err(`Unable to send message: ${ err }`);
         }
