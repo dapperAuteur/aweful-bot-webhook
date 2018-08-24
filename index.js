e6b5b37ed3595e405ee4e5f38a1a51b4c64ab025
dapperauteur <a@awews.com>
2018-08-24 14:19:41 -0400

    add received_message.attachments code

--- index.js
+++ index.js
@@ -17,6 +17,33 @@ function handleMessage(sender_psid, received_message) {
         response = {
             "text": `You sent the message: "${ received_message.text }". Now send me an image!`
         }
+    } else if (received_message.attachments) {
+        let attachments_url = received_message.attachments[0].payload.url;
+        response = {
+            "attachment": {
+                "type": "template",
+                "payload": {
+                    "template_type": "generic",
+                    "elements": [{
+                        "title": "Is this the right picture?",
+                        "subtitle": "Tap a button to answer.",
+                        "image_url": attachments_url,
+                        "buttons": [
+                            {
+                                "type": "postback",
+                                "title": "Yes!",
+                                "payload": "yea"
+                            },
+                            {
+                                "type": "postback",
+                                "title": "No!",
+                                "payload": "nope"
+                            }
+                        ]
+                    }]
+                }
+            }
+        }
     }
     callSendAPI(sender_psid, response);
 }
