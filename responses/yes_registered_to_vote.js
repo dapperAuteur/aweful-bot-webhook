2d9a0aba9c08ff36f55c2017a9c6cf06dd065932
dapperauteur <a@awews.com>
2018-08-26 16:11:17 -0400

    add text to subtitle parameter to yes_registered_to_vote

--- responses/yes_registered_to_vote.js
+++ responses/yes_registered_to_vote.js
@@ -6,7 +6,7 @@ let yes_registered_to_vote = {
         "elements":[
           {
             "title":"You're doing amazing sweetie",
-            "subtitle":"🔥🔥🔥 You’re already halfway there! All that’s left now is to vote.",
+            "subtitle":"🔥🔥🔥 You’re already halfway there! All that’s left now is to vote. I’ll message you a reminder when we get closer… you know, ‘cause I’m a good friend like that. Now, why don’t you level up again by challenging six of your friends to take the #sixbysix challenge along with you?",
             "image_url":"https://firebasestorage.googleapis.com/v0/b/js-react-todo-0002.appspot.com/o/youre_doing_great_kard.gif?alt=media&token=d789be1c-fcc9-45fc-a7a0-57cab22ebf92",
             // "text": "I’ll message you a reminder when we get closer… you know, ‘cause I’m a good friend like that. Now, why don’t you level up again by challenging six of your friends to take the #sixbysix challenge along with you?",
             "default_action": {
