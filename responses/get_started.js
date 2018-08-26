27ca61d9832024e4c10a5fae5015eee5069b8044
dapperauteur <a@awews.com>
2018-08-25 20:31:52 -0400

    test get_started

--- responses/get_started.js
+++ responses/get_started.js
@@ -6,36 +6,11 @@ let get_started = {
       "elements":[
         {
           "text": "CONGRATS on accepting the #sixbysix challenge! Now, Level Up! Are you registered to vote?"
-      },
-      {
-        "title":"#SixBySix",
-        "image_url":"https://firebasestorage.googleapis.com/v0/b/js-react-todo-0002.appspot.com/o/challengeAccepted.png?alt=media&token=57d4fc02-73fb-4b9b-b2b2-c32ebc686e85",
-        "default_action": {
-          "type": "web_url",
-          "url": "https://js-react-todo-0002.firebaseapp.com/"
-        },
-        "buttons":[
-                    {
-                        "type": "postback",
-                        "title": "Yes!",
-                        "payload": "yes"
-                    },
-                    {
-                        "type": "postback",
-                        "title": "No, I haven't yet",
-                        "payload": "No, I haven't yet"
-                    },
-                    {
-                        "type": "postback",
-                        "title": "I don't know",
-                        "payload": "I don't know"
-                    }
-                ]      
-        },
+      }
       ]
     }
   }
-}
+};
 
 // let get_started = {
   // "attachment":{
