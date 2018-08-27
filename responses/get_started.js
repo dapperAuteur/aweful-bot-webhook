03ebe0b83ad31397504d98cc0ea928992621d6c1
dapperauteur <a@awews.com>
2018-08-27 14:35:40 -0400

    refactor i_already_did_it_1I for spacing, and get_started with image

--- responses/get_started.js
+++ responses/get_started.js
@@ -1,29 +1,45 @@
-let get_started = {
-  "attachment": {
-    "type": "template",
-    "payload": {
-      "template_type":"button",
-      "text": "Congrats on accepting the #sixbysix challenge! Now, Level Up! Are you registered to vote?",
-      "buttons":[
-        {
-            "type": "postback",
-            "title": "Yes!",
-            "payload": "Yes!"
-        },
-        {
-            "type": "postback",
-            "title": "No, I haven't yet",
-            "payload": "No, I haven't yet"
-        },
-        {
-            "type": "postback",
-            "title": "I don't know",
-            "payload": "I don't know"
-        }
-      ]  
+let get_started = [
+  {
+    "text": "Congrats on accepting the #sixbysix challenge! Now, Level Up! Are you registered to vote?",
+    "buttons"
+  },
+  {
+    "attachment": {
+      "type": "template",
+      "payload": {
+        "template_type":"generic",
+        "elements": [
+          {
+            "title":"#SixBySix Challenge Accepted",
+            "subtitle":"SixBySix Fb Page",
+            "image_url":"https://firebasestorage.googleapis.com/v0/b/js-react-todo-0002.appspot.com/o/challengeAccepted.png?alt=media&token=57d4fc02-73fb-4b9b-b2b2-c32ebc686e85",
+            "default_action": {
+                "type": "web_url",
+                "url": "https://www.facebook.com/sixbysixvote/"
+            } 
+          }
+        ],
+        "buttons":[
+          {
+              "type": "postback",
+              "title": "Yes!",
+              "payload": "Yes!"
+          },
+          {
+              "type": "postback",
+              "title": "No, I haven't yet",
+              "payload": "No, I haven't yet"
+          },
+          {
+              "type": "postback",
+              "title": "I don't know",
+              "payload": "I don't know"
+          }
+        ]  
+      }
     }
   }
-}
+]
 
 // let get_started = {
   // "attachment":{
--- responses/i_already_did_1I.js
+++ responses/i_already_did_1I.js
@@ -8,15 +8,15 @@ let i_already_did_1I = [
             "payload":{
                 "template_type":"generic",
                 "elements":[
-                {
-                    "title":"That's my bad. That's on me.",
-                    "subtitle":"SixBySix Fb Page",
-                    "image_url":"https://firebasestorage.googleapis.com/v0/b/js-react-todo-0002.appspot.com/o/the_flash_cisco-thats_my_bad.gif?alt=media&token=ed9bf4be-1a1c-46c1-b263-b954f4b68844",
-                    "default_action": {
-                        "type": "web_url",
-                        "url": "https://www.facebook.com/sixbysixvote/"
-                    } 
-                }
+                    {
+                        "title":"That's my bad. That's on me.",
+                        "subtitle":"SixBySix Fb Page",
+                        "image_url":"https://firebasestorage.googleapis.com/v0/b/js-react-todo-0002.appspot.com/o/the_flash_cisco-thats_my_bad.gif?alt=media&token=ed9bf4be-1a1c-46c1-b263-b954f4b68844",
+                        "default_action": {
+                            "type": "web_url",
+                            "url": "https://www.facebook.com/sixbysixvote/"
+                        } 
+                    }
                 ]
             }
         }
